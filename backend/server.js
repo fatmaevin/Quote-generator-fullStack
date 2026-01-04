7c9cd0b237c2b7f996da9ca7676e01eec52b822a
Fatma Degirmenci <fatosdegirmenci644@gmilcom>
2026-01-04 16:24:40 +0000

    Update backend

--- backend/server.js
+++ backend/server.js
@@ -77,6 +77,7 @@ app.post("/", (req, res) => {
   res.send("ok");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
